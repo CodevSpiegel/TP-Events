3cce83ecf83e700390edc49b5bc92e3482ca4ad7
Zalumetkolo <zalumetkolo@gmail.com>
2025-06-01 21:00:15 +0200

    WIP

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ define( 'ABSPATH', __DIR__ . '/' );
 
 error_reporting( E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_ERROR | E_WARNING | E_PARSE | E_USER_ERROR | E_USER_WARNING | E_RECOVERABLE_ERROR );
 
+require_once ABSPATH . "config.php";
+
 //--------------------------------
 // Import GLOBAL FUNCTIONS !
 //--------------------------------
